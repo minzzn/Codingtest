055b17fa9313fb3bc05b4397e5940cfa7a8e1f22
김민주 <97500865+minzzn@users.noreply.github.com>
2024-05-24 16:10:05 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 21676 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,34 +1,27 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 let n = input[0];
 let m = input[1];
-function solution(n, m) {
-  // 순열을 계산하고자 하는 원소가 담긴 배열 [1, 2, 3, 4]
-  let arr = Array.from({ length: n }, (v, i) => i + 1);
-  // 각 원소의 인덱스 별 방문 여부
-  let visited = new Array(n).fill(false);
-  // 현재 순열에 포함된 원소
-  let selected = [];
 
+function solution(n, m) {
+  let arr = Array.from({ length: n }, (v, i) => i + 1);
+  let visited = new Array(n).fill(false);
   let answer = "";
-  function dfs(arr, depth) {
-    // 모든 순열을 확인하는 부분
+
+  function dfs(arr, depth, currentPermutation) {
     if (depth === m) {
-      let result = []; // 순열 결과 저장
-      for (let i of selected) result.push(arr[i]);
-      for (let x of result) answer += x + " ";
-      answer += "\n"; // 줄바꿈 처리
+      answer += currentPermutation.join(" ") + "\n";
       return;
     }
+
     for (let i = 0; i < arr.length; i++) {
-      if (visited[i]) continue; // 이미 처리된 원소라면 무시하고 넘어감
-      selected.push(i); // 현재 원소 선택
-      visited[i] = true; // 방문처리
-      dfs(arr, depth + 1); // 재귀함수 호출
-      selected.pop(); // 현재 원소 선택 취소
-      visited[i] = false; // 원소 방문 처리 취소
+      if (visited[i]) continue;
+      visited[i] = true;
+      dfs(arr, depth + 1, currentPermutation.concat(arr[i]));
+      visited[i] = false;
     }
   }
-  dfs(arr, 0);
+
+  dfs(arr, 0, []);
   return answer;
 }
 
